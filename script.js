f3829560fd0b16d417b10c1b16a49e80a5b0b028
Anshuljain05 <anshuljain05@outlook.com>
2022-10-09 00:48:38 +0530

    Pagination added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    var $pagination = $("#pagination"),
+      totalRecords = 0,
+      records = [],
+      recPerPage = 0,
+      nextPageToken = "",
+      totalPages = 0;
     var API_KEY = "";
     var search = "";
     var maxResults=10;    
@@ -26,12 +32,53 @@ $(document).ready(function () {
         },
       });
     });
+
+    function apply_pagination() {
+      $pagination.twbsPagination({
+        totalPages: totalPages,
+        visiblePages: 6,
+        onPageClick: function (event, page) {
+          console.log(event);
+          displayRecordsIndex = Math.max(page - 1, 0) * recPerPage;
+          endRec = displayRecordsIndex + recPerPage;
+          console.log(displayRecordsIndex + "ssssssssss" + endRec);
+          displayRecords = records.slice(displayRecordsIndex, endRec);
+          generateRecords(recPerPage, nextPageToken);
+        },
+      });
+    }
     
     $("#search").change(function () {
       search = $("#search").val();
     });
+
+    function generateRecords(recPerPage, nextPageToken) {
+      var url2 = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&q=${search}&maxResults=${maxResults}&pageToken=${nextPageToken}&type=video`;
+  
+      $.ajax({
+        method: "GET",
+        url: url2,
+        beforeSend: function () {
+          $("#btn").attr("disabled", true);
+          $("#results").empty();
+        },
+        success: function (data) {
+          console.log(data);
+          $("#btn").attr("disabled", false);
+          displayVideos(data);
+        },
+      });
+    }
   
     function displayVideos(data) {
+  
+      recPerPage = data.pageInfo.resultsPerPage;
+      nextPageToken = data.nextPageToken;
+      console.log(records);
+      totalRecords = data.pageInfo.totalResults;
+      totalPages = Math.ceil(totalRecords / recPerPage);
+      apply_pagination();
+      $("#search").val("");
   
       var videoData = "";
   
